c167ed9466c41d4cebf8df806024f4350a8f8d18
braden <leachbr787@northwested.org>
2025-02-03 13:08:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+#Braden Leach
+#Febuary 3 2025
+#Dictonary Evidence
+
+
+#---Dictonary---#
+fav_artists = {
+            'Bruno Mars':'Die for you',
+            'Imagine Dragons':'Beliver',
+            'Chase Atlantic': 'Swim',
+            'Jorge Herrans': 'Remember Them',
+            'The Weekend':'Star boy',
+            'Arctic Monkey':'I wanna be yours'
+}
+
+#-Print-#
+print(f'{fav_artists}\n')
+
+#--Update--#
+fav_artists.update({'Lady Gaga':'Judas'})
+
+#-Print-#
+print(fav_artists)
+
+
+#---Loop Dict---#
+#-keys-#
+for key in fav_artists.keys(): 
+    print(f'{key}\n')
+#-values-#
+for value in fav_artists.values():
+    print(f'{value}\n')
+#-Pair-#
+for items in fav_artists.items():
+    print(f'{items}\n')
+
 
